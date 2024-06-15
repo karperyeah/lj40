35d7da12b7c644735810d12e5fb12d69dd130a47
karper, yeah <karperyeah@gmail.com>
2024-06-15 03:58:27 +0200

    Add character controller, for real this time.

---
+++ assets/scripts/player.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+onready var sprite = $Sprite
+onready var animation_player = $AnimationPlayer
+
+export var gravity = 480.0
+
+export var speed = 150.0
+export var acceleration = 600.0
+export var deacceleration = 400.0
+
+export var jump_velocity = 200.0
+
+var direction = 0.0
+var velocity = Vector2()
+
+
+func _gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+
+func _movement(delta: float) -> void:
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, deacceleration * delta)
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	
+func _jump(delta: float) -> void:
+	if Input.is_action_pressed("action") and is_on_floor():
+		velocity.y = -jump_velocity
+
+
+func _animation(direction: float) -> void:
+	if direction:
+		sprite.flip_h = direction < 0
+	else:
+		animation_player.play("Idle")
+
+
+func _process(delta) -> void:
+	direction = Input.get_axis("left", "right")
+	
+	_animation(direction)
+	_jump(delta)
+	
+
+func _physics_process(delta: float) -> void:
+	_gravity(delta)
+	_movement(delta)
