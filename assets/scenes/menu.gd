98085a6d9a3efe4fd73e08b0728fa75e7bf3221c
mechanicalruby <robbierocket05@gmail.com>
2024-06-16 14:10:34 -0700

    Explosion fx, door explode cutscene

--- assets/scenes/menu.gd
+++ assets/scenes/menu.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var explosion_effect_scene = preload("res://assets/scenes/fx/explosion.tscn")
+
 func _ready():
 	$TV.visible = false
 
@@ -7,7 +9,17 @@ func _process(delta):
 	pass
 
 func _on_play_button_press():
-	$AnimationPlayer.play("IntroCutscene")
+	$AnimationPlayer.play("DoorExplodeCutscene")
 
 func _on_settings_button_press():
 	pass # Replace with function body.
+
+func _spawn_explosion(): # literally just for the opening cutscene
+	var explosion = explosion_effect_scene.instance()
+	explosion.position = $Door.position
+	add_child(explosion)
+	$Player.position = $Door.position
+	print("Boom!")
+
+func _shake_screen(amount : float):
+	Globals.shakyness += amount
